22334b9bf51fc77d6f1f7f4f2cc85d7f1de6742d
SebastianeTapnio <sebastiane.tapnio130274@my.lsgh.edu.ph>
2021-04-29 10:16:10 +0800

    1 file modified

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -19,13 +19,13 @@ export class HomePage {
   calculateBMR() {
     this.bmrmale = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
     this.bmrmale = parseFloat(this.bmrmale.toFixed(2));
-    this.bmrfemale = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161
+    this.bmrfemale = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
     this.bmrfemale = parseFloat(this.bmrfemale.toFixed(2));
 
     if (this.bmrmale) {
      this.bmrmale = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
     } else {
-      this.bmrfemale = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161
+      this.bmrfemale = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
     }
   }
 
