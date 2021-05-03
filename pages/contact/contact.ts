40fc5f3126d49fd496cf171219093a695881f36e
SebastianeTapnio <sebastiane.tapnio130274@my.lsgh.edu.ph>
2021-05-03 14:00:19 +0800

    1 file modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -12,9 +12,21 @@ height: number;
   bmrmale: number;
   bmrfemale: number;
   bmrMessage: string;
-  
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController) {
+  }
+CalculateBMR() {
+    this.bmrmale = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
+    this.bmrmale = parseFloat(this.bmrmale.toFixed(2));
+    this.bmrfemale = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
+    this.bmrfemale = parseFloat(this.bmrfemale.toFixed(2));
+
+    if (this.bmrmale) {
+     this.bmrmale = 10 * this.weight + 6.25 * this.height - 5 * this.age + 5;
+    } else {
+      this.bmrfemale = 10 * this.weight + 6.25 * this.height - 5 * this.age - 161;
+    }
   }
 
 }
+
