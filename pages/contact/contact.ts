1a5c4fdd483869cf1778f0aeac1dc865fdeade25
SebastianeTapnio <sebastiane.tapnio130274@my.lsgh.edu.ph>
2021-05-03 13:58:15 +0800

    1 file modified

--- pages/contact/contact.ts
+++ pages/contact/contact.ts
@@ -6,7 +6,13 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
-
+height: number;
+  weight: number;
+  age: number;
+  bmrmale: number;
+  bmrfemale: number;
+  bmrMessage: string;
+  
   constructor(public navCtrl: NavController) {
 
   }
